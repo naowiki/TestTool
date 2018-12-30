faf9280635dd9746605b16c3a295652954f802ae
nao@home <nao.home@outlook.com>
2018-12-30 19:51:58 +0900

    ドラッグ＆ドロップを受け付けるように変更

--- TestTool/TestTool.cpp
+++ TestTool/TestTool.cpp
@@ -4,4 +4,21 @@ TestTool::TestTool(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	setAcceptDrops(true);
+}
+
+void TestTool::dragEnterEvent(QDragEnterEvent *e)
+{
+	if (e->mimeData()->hasFormat("text/uri-list"))
+	{
+		//視覚的にドロップを受付られることを表示し、
+		//ドラッグ＆ドロップを受け付ける。（これがないと受付られない）
+		e->acceptProposedAction();
+	}
+}
+
+void TestTool::dropEvent(QDropEvent *e)
+{
+	//dragEnterEventの後にくるイベント
+	//ドロップの際の動作を記述する
 }
--- TestTool/TestTool.h
+++ TestTool/TestTool.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QDropEvent>
+#include <QMimeData>
+#include <QUrl>
 #include "ui_TestTool.h"
 
 class TestTool : public QMainWindow
@@ -9,6 +12,8 @@ class TestTool : public QMainWindow
 
 public:
 	TestTool(QWidget *parent = Q_NULLPTR);
+	void dragEnterEvent(QDragEnterEvent *e);
+	void dropEvent(QDropEvent *e);
 
 private:
 	Ui::TestToolClass ui;

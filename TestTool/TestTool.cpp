f74c53beb3a9993178ba68342d83570fb8d8e71f
fukuda.naotaka@mac <nao.home@outlook.com>
2019-01-06 23:58:14 +0900

    下記修正 - insertRow引数変更 - テーブル列の初期化タイミング変更 - タブを半角スペース4つに変更

--- TestTool/TestTool.cpp
+++ TestTool/TestTool.cpp
@@ -2,100 +2,106 @@
 
 enum eColumnType
 {
-	COLUMN_APP		= 0,
-	COLUMN_CASE		= 1,
-	COLUMN_SUITE	= 2,
-	COLUMN_RESULT	= 3,
-	COLUMN_LOG		= 4,
-	COLUMN_OTHER	= 5
+    COLUMN_APP    = 0,
+    COLUMN_CASE   = 1,
+    COLUMN_SUITE  = 2,
+    COLUMN_RESULT = 3,
+    COLUMN_LOG    = 4,
+    COLUMN_OTHER  = 5
 };
 
-TestTool::TestTool(QWidget *parent)
-    : QMainWindow(parent),
-      m_nRowCount(0)
+TestTool::TestTool( QWidget *parent )
+    : QMainWindow( parent ),
+      m_nRowCount( 0 )
 {
-	ui.setupUi(this);
-	setAcceptDrops(true);
+    ui.setupUi( this );
+    setAcceptDrops( true );
     this->resize( this->width(), this->height() - 320 );
 
-    connect( ui.refBtn, SIGNAL( clicked() ),
-             this,      SLOT( slotRefBtnClicked() ) );
-    connect( ui.analyzeBtn, SIGNAL( clicked() ),
-             this,          SLOT( slotAnalyzeBtnClicked() ) );
-	connect( ui.tableWidget, SIGNAL(cellDoubleClicked(int, int)),
-			 this,			 SLOT(tableItemClicked(int, int)));
-	connect( ui.tableWidget, SIGNAL(currentCellChanged(int, int, int, int)),
-			 this,			 SLOT(tableItemChanged(int, int, int, int)));
-    connect( ui.extBtn, SIGNAL( clicked() ),
-             this,      SLOT( slotExtBtnClicked() ) );
-    connect( ui.failChkBox, SIGNAL( stateChanged(int) ),
-             this,      SLOT( slotFailChkBoxChanged() ) );
+    connect( ui.refBtn,      SIGNAL( clicked() ),
+             this,           SLOT( slotRefBtnClicked() ) );
+    connect( ui.analyzeBtn,  SIGNAL( clicked() ),
+             this,           SLOT( slotAnalyzeBtnClicked() ) );
+    connect( ui.tableWidget, SIGNAL( cellDoubleClicked( int, int ) ),
+             this,           SLOT( tableItemClicked( int, int ) ) );
+    connect( ui.tableWidget, SIGNAL( currentCellChanged( int, int, int, int ) ),
+             this,           SLOT( tableItemChanged( int, int, int, int) ) );
+    connect( ui.extBtn,      SIGNAL( clicked() ),
+             this,           SLOT( slotExtBtnClicked() ) );
+    connect( ui.failChkBox,  SIGNAL( stateChanged( int ) ),
+             this,           SLOT( slotFailChkBoxChanged() ) );
 
-	initTable();
-    ui.tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers); // 編集禁止
+    initTable();
+    ui.tableWidget->setEditTriggers( QAbstractItemView::NoEditTriggers ); // 編集禁止
 }
 
-void TestTool::dragEnterEvent(QDragEnterEvent *e)
+void TestTool::dragEnterEvent( QDragEnterEvent *e )
 {
-	if (e->mimeData()->hasFormat("text/uri-list"))
-	{
-		//視覚的にドロップを受付られることを表示し、
-		//ドラッグ＆ドロップを受け付ける。（これがないと受付られない）
-		e->acceptProposedAction();
-	}
-}
-
-void TestTool::dropEvent(QDropEvent *e)
-{
-	//dragEnterEventの後にくるイベント
-	//ドロップの際の動作を記述する
-	ui.filePathLineEdit->setText(e->mimeData()->urls().first().toLocalFile());
-    l.clear();
-    if (analyzeXml())
+    if ( e->mimeData()->hasFormat("text/uri-list") )
     {
+        //視覚的にドロップを受付られることを表示し、
+        //ドラッグ＆ドロップを受け付ける。（これがないと受付られない）
+        e->acceptProposedAction();
+    }
+}
+
+void TestTool::dropEvent( QDropEvent *e )
+{
+    //dragEnterEventの後にくるイベント
+    //ドロップの際の動作を記述する
+    ui.filePathLineEdit->setText( e->mimeData()->urls().first().toLocalFile() );
+    if ( analyzeXml() )
+    {
+        initTable();
         updateTableRow();
-	}
+    }
 }
 
 void TestTool::initTable()
 {
     m_nRowCount = 0;
-	ui.tableWidget->setColumnCount( 6 );
-	ui.tableWidget->setRowCount( 0 );
+    ui.tableWidget->setColumnCount( 6 );
+    ui.tableWidget->setRowCount( 0 );
 }
 
-void TestTool::insertRow(int nRow, int nColumn, QString item)
+void TestTool::insertRow( logdata_t log )
 {
-	ui.tableWidget->setItem( nRow, nColumn, new QTableWidgetItem(item));
+    ui.tableWidget->setRowCount( m_nRowCount + 1 );
+    ui.tableWidget->setItem( m_nRowCount, COLUMN_APP,    new QTableWidgetItem( log.strApp ) );
+    ui.tableWidget->setItem( m_nRowCount, COLUMN_CASE,   new QTableWidgetItem( log.strCase ) );
+    ui.tableWidget->setItem( m_nRowCount, COLUMN_SUITE,  new QTableWidgetItem( log.strSuite ) );
+    ui.tableWidget->setItem( m_nRowCount, COLUMN_RESULT, new QTableWidgetItem( log.strResult ) );
+    ui.tableWidget->setItem( m_nRowCount, COLUMN_LOG,    new QTableWidgetItem( log.strLog ) );
+    ui.tableWidget->setItem( m_nRowCount, COLUMN_OTHER,  new QTableWidgetItem( log.strOther ) );
+    m_nRowCount++;
 }
 
 bool TestTool::analyzeXml()
 {
-    initTable();
-
-	QString filePath;
-	filePath = ui.filePathLineEdit->text();
-	QFile file(filePath);
-	if (!file.open(QFile::ReadOnly | QFile::Text))
-	{
-		return false;
-	}
-
-	QDomDocument domDocument;
-	domDocument.setContent(&file);
-
-	QDomNodeList domList = domDocument.elementsByTagName("item");
-	for ( int i = 0; i < domList.count(); i++ )
-	{
-		l.push_back( makeNewLogData( "",
-									 "",
-									 domList.at(i).toElement().attributeNode("id").value(),
-									 domList.at(i).firstChildElement("result").text(),
-									 domList.at(i).firstChildElement("name").text(),
-									 "" ) );
+    QString filePath;
+    filePath = ui.filePathLineEdit->text();
+    QFile file( filePath );
+    if ( !file.open(QFile::ReadOnly | QFile::Text) )
+    {
+        return false;
     }
 
-	return true;
+    QDomDocument domDocument;
+    domDocument.setContent( &file );
+
+    QDomNodeList domList = domDocument.elementsByTagName( "item" );
+    l.clear();
+    for ( int i = 0; i < domList.count(); i++ )
+    {
+        l.push_back( makeNewLogData( "",
+                                     "",
+                                     domList.at( i ).toElement().attributeNode( "id" ).value(),
+                                     domList.at( i ).firstChildElement( "result" ).text(),
+                                     domList.at( i ).firstChildElement( "name" ).text(),
+                                     "" ) );
+    }
+
+    return true;
 }
 
 void TestTool::slotRefBtnClicked()
@@ -105,39 +111,39 @@ void TestTool::slotRefBtnClicked()
         this,
         "ファイルを選択",
         "/Users/nao/Desktop/",
-        tr("*.xml"),
+        tr( "*.xml" ),
         &selFilter,
         QFileDialog::DontUseCustomDirectoryIcons
     );
 
-    if (fileName.isEmpty()) {
+    if ( fileName.isEmpty() ) {
         // キャンセル
     } else {
-        ui.filePathLineEdit->setText(fileName);
+        ui.filePathLineEdit->setText( fileName );
     }
 }
 
 void TestTool::slotAnalyzeBtnClicked()
 {
-    if (!ui.filePathLineEdit->text().isEmpty())
+    if ( !ui.filePathLineEdit->text().isEmpty() )
     {
-        l.clear();
-        if (analyzeXml())
+        if ( analyzeXml() )
         {
+            initTable();
             updateTableRow();
         }
     }
 }
 
-logdata_t TestTool::makeNewLogData(QString app, QString tstcase, QString suite, QString result, QString log, QString other)
+logdata_t TestTool::makeNewLogData( QString app, QString tstcase, QString suite, QString result, QString log, QString other )
 {
     logdata_t log_temp;
-	log_temp.strApp = app;
-	log_temp.strCase = tstcase;
-	log_temp.strSuite = suite;
+    log_temp.strApp    = app;
+    log_temp.strCase   = tstcase;
+    log_temp.strSuite  = suite;
     log_temp.strResult = result;
-    log_temp.strLog = log;
-	log_temp.strOther = other;
+    log_temp.strLog    = log;
+    log_temp.strOther  = other;
 
     return log_temp;
 }
@@ -145,47 +151,35 @@ logdata_t TestTool::makeNewLogData(QString app, QString tstcase, QString suite, 
 void TestTool::updateTableRow()
 {
     m_nRowCount = 0;
-    for (int i = 0; i < l.count(); i++)
+    for ( int i = 0; i < l.count(); i++ )
     {
-        if (ui.failChkBox->isChecked())
+        if ( ui.failChkBox->isChecked() )    // FAILのみ表示させる場合
         {
-            if("FAIL"==l.at(i).strResult)
+            if( "FAIL" == l.at( i ).strResult )
             {
-                ui.tableWidget->setRowCount(m_nRowCount + 1);
-                insertRow(m_nRowCount, COLUMN_APP,    l.at(i).strApp);
-                insertRow(m_nRowCount, COLUMN_CASE,   l.at(i).strCase);
-                insertRow(m_nRowCount, COLUMN_SUITE,  l.at(i).strSuite);
-                insertRow(m_nRowCount, COLUMN_RESULT, l.at(i).strResult);
-                insertRow(m_nRowCount, COLUMN_LOG,    l.at(i).strLog);
-                insertRow(m_nRowCount, COLUMN_OTHER,  l.at(i).strOther);
-                m_nRowCount++;
+                insertRow( l.at( i ) );
             }
         }
-        else
+        else                                 // すべて表示させる場合
         {
-            ui.tableWidget->setRowCount(i + 1);
-            insertRow(i, COLUMN_APP,    l.at(i).strApp);
-            insertRow(i, COLUMN_CASE,   l.at(i).strCase);
-            insertRow(i, COLUMN_SUITE,  l.at(i).strSuite);
-            insertRow(i, COLUMN_RESULT, l.at(i).strResult);
-            insertRow(i, COLUMN_LOG,    l.at(i).strLog);
-            insertRow(i, COLUMN_OTHER,  l.at(i).strOther);
+            insertRow( l.at( i ) );
         }
     }
+
     emit slotImageUpdated( 0 );
 }
 
-void TestTool::tableItemClicked(int nRow, int nCol)
+void TestTool::tableItemClicked( int nRow, int nCol )
 {
 
 }
 
-void TestTool::tableItemChanged(int currentRow, int currentColumn, int previousRow, int previousColumn)
+void TestTool::tableItemChanged( int currentRow, int currentColumn, int previousRow, int previousColumn )
 {
-    emit slotImageUpdated(currentColumn);
+    emit slotImageUpdated( currentColumn );
 }
 
-void TestTool::slotImageUpdated(int nCol)
+void TestTool::slotImageUpdated( int nCol )
 {
     QImage *mImage = new QImage();
     mImage->load( "/Users/nao/Desktop/mario.png" );
@@ -197,15 +191,15 @@ void TestTool::slotImageUpdated(int nCol)
 
 void TestTool::slotExtBtnClicked()
 {
-    if ( ">>" ==ui.extBtn->text() )
+    if ( ">>" == ui.extBtn->text() )
     {
         this->resize( this->width(), this->height() + 320 );
-        ui.extBtn->setText("<<");
+        ui.extBtn->setText( "<<" );
     }
     else
     {
         this->resize( this->width(), this->height() - 320 );
-        ui.extBtn->setText(">>");
+        ui.extBtn->setText( ">>" );
     }
 }
 
--- TestTool/TestTool.h
+++ TestTool/TestTool.h
@@ -20,19 +20,19 @@ struct logdata_t {
 
 class TestTool : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	TestTool(QWidget *parent = Q_NULLPTR);
-	void dragEnterEvent(QDragEnterEvent *e);
-	void dropEvent(QDropEvent *e);
+    TestTool( QWidget *parent = Q_NULLPTR );
+    void dragEnterEvent( QDragEnterEvent *e );
+    void dropEvent( QDropEvent *e );
 
 private:
-	Ui::TestToolClass ui;
-	void initTable();
-	void insertRow(int row, int column, QString item);
+    Ui::TestToolClass ui;
+    void initTable();
+    void insertRow( logdata_t log );
     bool analyzeXml();
-    logdata_t makeNewLogData(QString app, QString tstcase, QString suite, QString result, QString log, QString other);
+    logdata_t makeNewLogData( QString app, QString tstcase, QString suite, QString result, QString log, QString other );
     void updateTableRow();
 
     int m_nRowCount;
@@ -41,9 +41,9 @@ private:
 private slots:
     void slotRefBtnClicked();
     void slotAnalyzeBtnClicked();
-	void tableItemClicked(int nRow, int nCol);
-	void tableItemChanged(int currentRow, int currentColumn, int previousRow, int previousColumn);
-    void slotImageUpdated(int nCol);
+    void tableItemClicked( int nRow, int nCol );
+    void tableItemChanged( int currentRow, int currentColumn, int previousRow, int previousColumn );
+    void slotImageUpdated( int nCol );
     void slotExtBtnClicked();
     void slotFailChkBoxChanged();
 };
